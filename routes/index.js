0a96372264d867b6e0de584c1663be2d39cc2e59
Tantowi Alif Feryansyah <tantowialif15@gmail.com>
2022-11-23 23:23:36 +0700

    challenge21 search

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,74 @@ var moment = require('moment');
 /* GET home page. */
 module.exports = function (db) {
   router.get('/', function (req, res, next) {
+
+    const params = []
+    let count = 1
+    const values = []
+
+    if (req.query.id && req.query.idcheck) {
+      params.push(`id = $${count++}`)
+      values.push(req.query.id)
+    }
+
+    if (req.query.string && req.query.stringcheck) {
+      params.push(`string ilike '%' || $${count++} || '%'`)
+      values.push(req.query.string)
+    }
+
+    if (req.query.integer && req.query.integercheck) {
+      params.push(`integer = $${count++}`)
+      values.push(req.query.integer)
+    }
+
+    if (req.query.float && req.query.floatcheck) {
+      params.push(`float = $${count++}`)
+      values.push(req.query.float)
+    }
+
+    if (req.query.datecheck) {
+      if (req.query.startdate != '' && req.query.enddate != '') {
+          params.push(`date BETWEEN $${count++} AND $${count++}`)
+          values.push(req.query.startdate)
+          values.push(req.query.enddate)
+      }
+      else if (req.query.startdate != '') {
+          params.push(`date >= $${count++}`)
+          values.push(req.query.startdate)
+      }
+      else if (req.query.enddate != '') {
+          params.push(`date <= $${count++}`)
+          values.push(req.query.enddate)
+      }
+  }
+
+    if (req.query.boolean && req.query.booleancheck) {
+      params.push(`boolean = $${count++}`)
+      values.push(req.query.booelan)
+    }
+
     const page = req.query.page || 1;
     const limit = 3;
     const offset = (page - 1) * limit
-    db.query(`SELECT COUNT(*) AS total FROM siswa`, (err,data) => {
+
+    let sql = `SELECT COUNT(*) AS total FROM siswa `
+    if (params.length > 0)
+      sql += `WHERE ${params.join(' AND ')}`
+
+    db.query(sql, values, (err, data) => {
       if (err) return console.log(`ini ${err}`);
       const total = data.rows[0].total
       const totalpages = Math.ceil(total / limit)
-      db.query(`SELECT * FROM siswa LIMIT $1 OFFSET $2`,[limit, offset], (err, data) => {
-        if (err) return console.log(`ini ${err}`);
-        res.render('list', { data: data.rows, moment, page, totalpages, offset });
-      })
+
+      sql = `SELECT * FROM siswa`
+      if (params.length > 0)
+        sql += ` WHERE ${params.join(' AND ')}`
+
+      sql += ` LIMIT $${count++} OFFSET $${count++} `
+        db.query(sql, [...values, limit, offset], (err, data) => {
+          if (err) return console.log(`ini ${err}`);
+          res.render('list', { data: data.rows, moment, page, totalpages, offset });
+        })
     })
   });
 
@@ -48,7 +105,7 @@ module.exports = function (db) {
 
   router.post('/edit/:id', function (req, res, next) {
     const index = req.params.id
-    db.query(`UPDATE siswa SET string = $1, integer = $2, float = $3, date = $4, boolean = $5 WHERE id = $6`, [req.body.string, req.body.integer, req.body.float, req.body.date, req.body.boolean,index], (err, data) => {
+    db.query(`UPDATE siswa SET string = $1, integer = $2, float = $3, date = $4, boolean = $5 WHERE id = $6`, [req.body.string, req.body.integer, req.body.float, req.body.date, req.body.boolean, index], (err, data) => {
       if (err) return console.log('gagal ambil data', err);
       res.redirect('/');
     })
